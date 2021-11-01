c9793e462e2c8a4dfd76274a0fc699b7a67d4369
Essi Suomalainen <essi.suomalainen@hyperisland.se>
2021-11-01 20:34:47 +0100

    set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { InfoCardComponent } from './components/info-card/info-card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBarComponent,
+    FooterComponent,
+    InfoCardComponent
   ],
   imports: [
     BrowserModule,
